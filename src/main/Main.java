ef9fc6644aee1fc8d6fa1e7e708e4868ed7ced03
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-09-01 14:03:41 +0100

    crud complet exo 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,63 @@
 package main;
+
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello JDBC");
-		
-		Connection conn=null;
-		
+		// System.out.println("Hello JDBC");
+
+		Connection conn = null;
+
 		try {
-			//Class.forName("com.mysql.jdbc.Driver");  // a utiliser avant JDBC 4
-			
-			// Driver se charge impliciment via l'instruction dans le fichier java.sql.Driver
+			// Class.forName("com.mysql.jdbc.Driver"); // a utiliser avant JDBC 4
+
+			// Driver se charge impliciment via l'instruction dans le fichier
+			// java.sql.Driver
 			// jdbc url : "jdbc:mysql://localhost:3306/isikabd2021","root",""
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/isikabd2021","root","");
-			Statement stmt = conn.createStatement(); //Objet permettant d'accepter des requettes SQL
-			
-			
-		
-			
-			// Une insertion
-			//int result = stmt.executeUpdate("insert into etudiant(nom,email) values('ali', 'mohamed.ali@gmail.com')");
-			int result = stmt.executeUpdate("delete from etudiant");
-			System.out.println(result); // 2
-			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/isikabd2021", "root", "");
+			Statement stmt = conn.createStatement(); // Objet permettant d'accepter des requettes SQL
+			/*
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println("Donner un nom");
+			String nom = sc.nextLine();
+
+			System.out.println("Donner son email");
+			String email = sc.nextLine();
+			 */
+			// int result = stmt.executeUpdate("insert into etudiant(nom,email)
+			// values('"+nom+"', '"+email+"')");
+
+			// 1-Une insertion
+			// int result = stmt.executeUpdate("insert into etudiant(nom,email)
+			// values('ali', 'mohamed.ali@gmail.com')");
+
+			// 2-la suppression
+			// int result = stmt.executeUpdate("delete from etudiant");
+
+			// 3-la mise à jour
+			// int result = stmt.executeUpdate("update etudiant set email='amine@gmail.com'
+			// where id=3");
+			// System.out.println("Insertion avec succès"); // 2
+
+			// 4-Affichage
+
+			ResultSet rs = stmt.executeQuery("select id, nom, email from etudiant");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name= rs.getString("nom");
+				String mail= rs.getString("email");
+				
+				System.out.println("ID : "+id+ " Nom : "+name +" Email : "+mail);
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(conn);
+		// System.out.println(conn);
 
 	}
 
